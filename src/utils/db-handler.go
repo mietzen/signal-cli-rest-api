52d581854b66aaf9503c61f3e2aea01540e3be01
Nils Stein <social.nst@mailbox.org>
2022-02-04 17:32:56 +0100

    Added sqlite as default DB, added getters, refactoring

--- src/utils/db-handler.go
+++ src/utils/db-handler.go
@@ -5,39 +5,86 @@ import (
 	"fmt"
 
 	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func GetDatabaseConnection() *sql.DB {
-	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		GetEnv("POSTGRES_HOST", ""),
-		GetEnv("POSTGRES_PORT", ""),
-		GetEnv("POSTGRES_USER", ""),
-		GetEnv("POSTGRES_PASSWORD", ""),
-		GetEnv("POSTGRES_DB", ""))
+var (
+	dbConnection *sql.DB
+)
 
-	db, err := sql.Open("postgres", psqlconn)
-	if err != nil {
-		panic(err)
+func getDatabaseConnection() *sql.DB {
+	if dbConnection == nil {
+		dbType := GetEnv("DB_TYPE", "sqlite3")
+		var dbSource string
+		switch dbType {
+		case "postgres":
+			dbSource = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+				GetEnv("DB_HOST", ""),
+				GetEnv("DB_PORT", ""),
+				GetEnv("DB_USER", ""),
+				GetEnv("DB_PASSWORD", ""),
+				GetEnv("DB_NAME", ""))
+		default:
+			// sqlite3
+			dbSource = "/home/message-archive.db"
+		}
+		var err error
+		dbConnection, err = sql.Open(dbType, dbSource)
+		if err != nil {
+			panic(err)
+		}
 	}
-	return db
+	return dbConnection
 }
 
-func PushReceivedMessageToDatabase(message string) error {
+func getLastMsgs(numberOfMessages int, table string) (string, error) {
+	if numberOfMessages == 0 {
+		numberOfMessages = 5
+	}
+	var jsonStr string
+	db := getDatabaseConnection()
+	rows, err := db.Query(`SELECT "data" FROM "$1" ORDER BY id DESC LIMIT $2;`, table, numberOfMessages)
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
 
-	db := GetDatabaseConnection()
-
-	insertStament := `insert into "received_messages"("data") values($1)`
-	_, e := db.Exec(insertStament, message)
-	defer db.Close()
-	return e
+	jsonStr = "["
+	i := 0
+	for rows.Next() {
+		var msg string
+		err = rows.Scan(&msg)
+		if err != nil {
+			return "", err
+		}
+		jsonStr += msg
+		if i != (numberOfMessages - 1) {
+			jsonStr += ","
+		}
+		i++
+	}
+	jsonStr += "]"
+	return jsonStr, err
 }
 
-func PushSendMessageToDatabase(message []byte) error {
+func pushMsgsToDB(message string, table string) error {
+	db := getDatabaseConnection()
+	_, err := db.Exec(`insert into "$1"("data") values($2)`, table, message)
+	return err
+}
 
-	db := GetDatabaseConnection()
+func PushReceivedMsgsToDB(message string) error {
+	return pushMsgsToDB(message, "received_messages")
+}
 
-	insertStament := `insert into "send_messages"("data") values($1)`
-	_, e := db.Exec(insertStament, message)
-	defer db.Close()
-	return e
+func PushSendMsgsToDB(message string) error {
+	return pushMsgsToDB(message, "send_messages")
+}
+
+func GetLastReceivedMsgs(numberOfMessages int) (string, error) {
+	return getLastMsgs(numberOfMessages, "received_messages")
+}
+
+func GetLastSendMsgs(numberOfMessages int) (string, error) {
+	return getLastMsgs(numberOfMessages, "send_messages")
 }
